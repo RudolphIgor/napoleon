a65efa186b286aa2c0386105092824acfb5bc1f6
Igor Rudolf <azazil47@gmail.com>
2023-06-28 11:41:56 +0700

    Настроено подключение к БД. Не работает nodemon с переменными окружения

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from 'express'; 
-const PORT = 3000;
+import dotenv from 'dotenv';
+dotenv.config();
 
+
+const PORT = 3000
 const app = express()
+
 
 app.listen(PORT, () => 
   console.log(`Server is started on port: ${PORT}`)
