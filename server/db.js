effaa1e85feb17eec5e1efd443fbd93624edc4a0
Igor Rudolf <azazil47@gmail.com>
2023-06-28 13:55:03 +0700

    Настроено подключение к БД без переменных среды

--- server/db.js
+++ server/db.js
@@ -1,12 +1,24 @@
 import { Sequelize } from "sequelize";
 
-module.exports = new Sequelize (
+// module.exports = new Sequelize (
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     dialect: "postgres",
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT
+//   }
+// )
+const sequelize = new Sequelize (
   process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  "postgres" ,
+  "FaQDBSxm",
   {
     dialect: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT
   }
 )
+
+export default sequelize;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 import express from 'express'; 
 import dotenv from 'dotenv';
+import sequelize from './db.js';
 dotenv.config();
 
 
 const PORT = 3000
 const app = express()
 
+const start  = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server is started on port: ${PORT}`))
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-app.listen(PORT, () => 
-  console.log(`Server is started on port: ${PORT}`)
-)
+start()
+
