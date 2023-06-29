1bf74299cfe77b2e83ea4fb3660f8ea38ae74531
Igor Rudolf <azazil47@gmail.com>
2023-06-29 08:50:28 +0700

    Заливка репы

--- server/db.js
+++ server/db.js
@@ -1,15 +1,5 @@
 import { Sequelize } from "sequelize";
 
-// module.exports = new Sequelize (
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASS,
-//   {
-//     dialect: "postgres",
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT
-//   }
-// )
 const sequelize = new Sequelize (
   process.env.DB_NAME,
   "postgres" ,
